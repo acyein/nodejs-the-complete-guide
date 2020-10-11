1d56e2fc43f0f95a775839083a2cdcdb2d82dd43
Yein <chianyein@gmail.com>
2020-10-11 23:44:07 +0800

    Add cart-item, order and order-item models

---
+++ 11-UnderstandingSequelize/models/order-item.js
@@ -0,0 +1,17 @@
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const OrderItem = sequelize.define('orderItem', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    quantity: {
+        type: DataTypes.INTEGER
+    }
+});
+
+module.exports = OrderItem;
---
+++ 11-UnderstandingSequelize/models/order.js
@@ -0,0 +1,14 @@
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const Order = sequelize.define('order', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Order;
